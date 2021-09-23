c7061d585e4a000e3e7af275edb882a2ca8c7336
Shirley <shircar16@gmail.com>
2021-09-22 22:11:35 -0500

    fix errors

--- opcode_functions.c
+++ opcode_functions.c
@@ -72,9 +72,9 @@ void pall_function(stack_t **stack, unsigned int line_number)
 
 void pint_function(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL)
+	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
