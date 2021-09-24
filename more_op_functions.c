df6241d3616244597e4cc9084754590f7efcb61e
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-23 19:29:24 -0500

    adding functions add and nop

---
+++ more_op_functions.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+ * add_function - adds the top two elements of the stack.
+ * @stack: pointer to the list
+ * @line_number: number of lines
+ *
+ * Return: nothing.
+ */
+void add_function(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp = NULL;
+stack_t *current = NULL;
+int len = 0;
+int sum = 0;
+	current = *stack;
+	while (current != NULL)
+{
+		len++;
+		current = current->next;
+	}
+	if (len < 2)
+{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+}
+temp = *stack;
+*stack = (*stack)->next;
+(*stack)->prev = NULL;
+sum = temp->n + (*stack)->n;
+(*stack)->n = sum;
+free(temp);
+}
+/**
+ * nop_function - does not do anythig.
+ * @stack: pointer to the list
+ * @line_number: number of lines
+ *
+ * Return: nothing.
+ */
+void nop_function(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
