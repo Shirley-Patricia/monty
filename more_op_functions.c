dca4d471dc76a061c6ca19670225cb82bcb17b61
IsabelaGomez101 <3379@holbertonschool.com>
2021-09-23 19:36:42 -0500

    adding function sub

--- more_op_functions.c
+++ more_op_functions.c
@@ -43,3 +43,37 @@ void nop_function(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+
+/**
+ * sub_function - adds the top two elements of the stack.
+ * @stack: pointer to the list
+ * @line_number: number of lines
+ *
+ * Return: nothing.
+ */
+void sub_function(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp = NULL;
+stack_t *current = NULL;
+int len = 0;
+int sub = 0;
+
+	current = *stack;
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+temp = *stack;
+*stack = (*stack)->next;
+(*stack)->prev = NULL;
+sub = (*stack)->n - temp->n;
+(*stack)->n = sub;
+free(temp);
+}
